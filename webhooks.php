57e38db7ecc7c1ab2b58e44bfaf2848c43b802e0
pholawatRie <41222811+pholawatRie@users.noreply.github.com>
2018-07-14 19:07:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -29,12 +29,12 @@ if (!is_null($events['events'])) {
 						foreach($xml->children() as $xmlChildren) { 
 						        $vdn =  $xmlChildren->VDN;
 								if ($vdn == $restString){
-									$text =  "VDN : " . $vdn . "<br>"
-											."VDN Name : " . $xmlChildren->VDNNAME . "<br>"
-											."Vector : " . $xmlChildren->VECTOR . "<br>"
-											."Whisper : " . $xmlChildren->WHISPER . "<br>"
-											."Skill(Vec) : " . $xmlChildren->SKILL1 . "<br>"
-											."Skill(1st) : " . $xmlChildren->FIRSTSKILL . "<br>"
+									$text =  "VDN : " . $vdn . <br>
+											."VDN Name : " . $xmlChildren->VDNNAME . <br>
+											."Vector : " . $xmlChildren->VECTOR . <br>
+											."Whisper : " . $xmlChildren->WHISPER . <br>
+											."Skill(Vec) : " . $xmlChildren->SKILL1 . <br>
+											."Skill(1st) : " . $xmlChildren->FIRSTSKILL . <br>
 											."Skill Priority : " . $xmlChildren->SKILL1_PRIOR;
 								}								
 						}
