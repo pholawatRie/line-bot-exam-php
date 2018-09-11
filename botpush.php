5d68bbc9f030defcdfe39aef0bc54ae87a0990d5
pholawatRie <41222811+pholawatRie@users.noreply.github.com>
2018-09-12 03:08:54 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,8 +4,8 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'Ybjohp9msZcShKwbVsMSTAeVuHhwUMWxI3saGnBRAgyAyxpE7YpoMGzfQ3VYEFTMwkFZ04xbXWw4vXMi+clNbi3o3HUevgP2vtpuH2gqtmkegi9fdHuTXIIZ9842JgUJoUQH/eOekkBPDqQcsTXv1QdB04t89/1O/w1cDnyilFU=';
-//$access_token = '8oRfnbe0ItXYT8UBxwPlwQyYJJJz8gluSZhgkhpXv2O8oRfnbe0ItXYT8UBxwPlwQyYJJJz8gluSZhgkhpXv2O";
+//$access_token = 'Ybjohp9msZcShKwbVsMSTAeVuHhwUMWxI3saGnBRAgyAyxpE7YpoMGzfQ3VYEFTMwkFZ04xbXWw4vXMi+clNbi3o3HUevgP2vtpuH2gqtmkegi9fdHuTXIIZ9842JgUJoUQH/eOekkBPDqQcsTXv1QdB04t89/1O/w1cDnyilFU=';
+$access_token = '8oRfnbe0ItXYT8UBxwPlwQyYJJJz8gluSZhgkhpXv2O8oRfnbe0ItXYT8UBxwPlwQyYJJJz8gluSZhgkhpXv2O';
 $channelSecret = '75622d12a17683724a9b5e1817ea7573';
 
 $pushID = 'Ub0d420627d3ef5afc9a71f92ad14f089';
